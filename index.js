c55da555f6f27a6abe96ae0160d4c605e9a5dedc
Renato <renatoalcantara2022@gmail.com>
2023-05-11 20:54:56 -0300

    Adicionado comando start e lançamento do bot.

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ require('dotenv').config()
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
+bot.launch();
+console.log("[-] Telegram started")
