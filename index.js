439f529cd53dc2ff2f8db483868108ef06064957
Renato <renatoalcantara2022@gmail.com>
2023-05-11 20:58:04 -0300

    Tratando desligameto do bot com SIGINT e SIGTERM

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.launch();
 console.log("[-] Telegram started")
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
