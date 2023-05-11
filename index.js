581f341829af29cdea439008640d09df322a1093
Renato <renatoalcantara2022@gmail.com>
2023-05-11 20:51:49 -0300

    Resetei o tudo

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const { Bot } = require("./bot");
+require('dotenv').config()
 
-const myBot = new Bot();
-
-myBot.setCommands([
-  {
-    command: "start",
-    description: "Inicio",
-  },
-  {
-    command: "recarregar",
-    description: "Adicionar saldo",
-  },
-  {
-    command: "instrucoes",
-    description: "Instruções de uso",
-  },
-  {
-    command: "faq",
-    description: "Nossa FAQ",
-  },
-  {
-    command: "termos",
-    description: "Termos de uso",
-  },
-]);
-
-myBot.launch();
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
